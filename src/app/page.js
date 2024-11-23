594c4151636fcc4545c0c5e7e6ec148fae4d9964
trevo <trevorius45@gmail.com>
2024-11-23 08:25:16 -0500

    server side rendered component

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,27 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
-export default function Home() {
+export default async function Home() {
+  console.log('Hello from the console');
+
+  // const getAsyncData = async () => {
+  // 'use server';
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+  const data = await response.json();
+  console.log(data);
+  // };
+
+  const asyncData = JSON.stringify(data);
+
+  // const asyncData = await getAsyncData();
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
+          src='/next.svg'
+          alt='Next.js logo'
           width={180}
           height={38}
           priority
@@ -19,27 +32,28 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+        <div>{asyncData}</div>
 
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src='/vercel.svg'
+              alt='Vercel logomark'
               width={20}
               height={20}
             />
             Deploy now
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
             className={styles.secondary}
           >
             Read our docs
@@ -48,42 +62,42 @@ export default function Home() {
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
+            src='/file.svg'
+            alt='File icon'
             width={16}
             height={16}
           />
           Learn
         </a>
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
-            src="/window.svg"
-            alt="Window icon"
+            src='/window.svg'
+            alt='Window icon'
             width={16}
             height={16}
           />
           Examples
         </a>
         <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
+            src='/globe.svg'
+            alt='Globe icon'
             width={16}
             height={16}
           />
