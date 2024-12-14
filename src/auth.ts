80d1ac493ec5ceeb2b1c7a5a83e155340c352441
trevo <trevorius45@gmail.com>
2024-12-14 16:02:12 -0500

    :sparkles: feat: refactor authentication logic and add password hashing utility
    - Simplified the authentication module by removing unnecessary TypeScript declarations for the session interface.
    - Updated the NextAuth configuration to include a custom sign-in page.
    - Introduced a new utility function for password hashing using SHA-256 in the newly created `users.ts` file.
    
    This commit enhances the authentication process and introduces secure password handling, improving overall security and maintainability of the codebase.

--- src/auth.ts
+++ src/auth.ts
@@ -1,25 +1,10 @@
-import NextAuth, { DefaultSession } from 'next-auth';
+import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 
-declare module 'next-auth' {
-  /**
-   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
-  interface Session {
-    user: {
-      /** The user's postal address. */
-      address: string;
-      /**
-       * By default, TypeScript merges new interface properties and overwrites existing ones.
-       * In this case, the default session user properties will be overwritten,
-       * with the new ones defined above. To keep the default session user properties,
-       * you need to add them back into the newly declared interface.
-       */
-    } & DefaultSession['user'];
-  }
-}
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  pages: {
+    signIn: '/login',
+  },
   providers: [
     Credentials({
       credentials: {
@@ -46,9 +31,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       },
     }),
   ],
-  pages: {
-    signIn: '/login',
-  },
+
   secret: process.env.AUTH_SECRET || 'your-development-secret-key',
   session: {
     strategy: 'jwt',
