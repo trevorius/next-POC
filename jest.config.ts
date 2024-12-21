fdf2438a84fe195fd52117a580825a449d55f467
trevo <trevorius45@gmail.com>
2024-12-21 10:48:16 -0500

    :wrench: feat(jest): Enhance Jest configuration and add type definitions
    - Updated Jest configuration to include additional test match patterns and coverage collection settings.
    - Introduced new VSCode settings for improved Jest integration and auto-running tests on file save.
    - Added custom type definitions for Jest to extend matchers with additional functionality.

--- jest.config.ts
+++ jest.config.ts
@@ -17,7 +17,22 @@ const config: Config = {
   transformIgnorePatterns: [
     'node_modules/(?!next-auth|@auth/core|@auth/core/providers/credentials|@auth/core/providers|next/dist/client/components/|next/dist/client/|next/dist/shared/lib/|next/dist/shared/|next/dist/pages/|@next/env|next/navigation|next/dist/compiled/|next/dist/server/|@next/env|next/package.json)',
   ],
-  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/types/**/*',
+    '!src/**/index.{js,jsx,ts,tsx}',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
 
 export default createJestConfig(config);
